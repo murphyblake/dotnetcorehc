ac8fd2f57bd30c097f42871bfeae2b3ebb43d65b
David Blake <david@murphyblake.uk>
2018-04-02 16:52:24 +0100

    Added MySQLCheck

---
+++ HealthCheckBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcorehc
+{
+    public static class HealthCheckBuilderExtensions
+    {
+        public static HealthCheckBuilder AddMySQLCheck(this HealthCheckBuilder builder, string name, string connectionstring)
+        {
+            builder.AddCheck(name, new MySQLHealthCheck(connectionstring));
+            return builder;
+        }
+
+        public static HealthCheckBuilder AddMySQLCheck(this HealthCheckBuilder builder, string name ,string connectionstring, TimeSpan cacheDuration)
+        {
+            builder.AddCheck(name, new MySQLHealthCheck(connectionstring), cacheDuration);
+            return builder;
+        }
+    }
+}
---
+++ MySQLHealthCheck.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnetcorehc
+{
+    public class MySQLHealthCheck : IHealthCheck
+    {
+        readonly string _connectionString = String.Empty;
+
+        public MySQLHealthCheck(string ConnectionString)
+        {
+            _connectionString = ConnectionString ?? throw new NullReferenceException("Connection string cannot be null");
+        }
+
+        public ValueTask<IHealthCheckResult> CheckAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            CheckStatus status = CheckStatus.Unknown;
+            string description = "UNKNOWN";
+
+            try
+            {
+                using (var checkConnection = new MySql.Data.MySqlClient.MySqlConnection(_connectionString))
+                {
+                    checkConnection.Open();
+                    status = checkConnection.State == System.Data.ConnectionState.Open ? CheckStatus.Healthy : CheckStatus.Unhealthy;
+                }
+                return new ValueTask<IHealthCheckResult>(HealthCheckResult.FromStatus(status, description));
+            }
+            catch (Exception e)
+            {
+                return new ValueTask<IHealthCheckResult>(HealthCheckResult.FromStatus(CheckStatus.Unhealthy, $"Exception {e.Message}"));
+            }
+            
+
+        }
+    }
+}
